6ed94bef13841c650b2fc8722fcac610f0fb6ddd
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-19 13:23:23 +0300

    Update Source.c

--- labs/lab2compress/Source.c
+++ labs/lab2compress/Source.c
@@ -94,22 +94,25 @@ void free_memory(struct stack* st1, struct stack* st2, int n)
 
 void print_in_file(FILE* fp, char* val1, char* val2, char* add, char c)
 {
-    if (strcmp(val2, add) != 0 && add != NULL) {
-        int z = 0;
-        while (add[z] != val2[0]) {
-            fprintf(fp, "%c", add[z]);
-            z++;
+    if (add != NULL) {
+        if (strcmp(val2, add) != 0) {
+            int z = 0;
+            while (add[z] != val2[0]) {
+                fprintf(fp, "%c", add[z]);
+                z++;
+            }
+            fprintf(fp, "%s", val1);
+            z = z + strlen(val2);
+            while (add[z] != '\n' && add[z] != '\0') {
+                fprintf(fp, "%c", add[z]);
+                z++;
+            }
         }
-        fprintf(fp, "%s", val1);
-        z = z + strlen(val2);
-        while (add[z] != '\n' && add[z] != '\0') {
-            fprintf(fp, "%c", add[z]);
-            z++;
+        else {
+            fprintf(fp, "%s", val1);
         }
     }
-    else {
-        fprintf(fp, "%s", val1);
-    }
+    
 
     if (c == '\n') { fprintf(fp, "%c", c); }
     else {
