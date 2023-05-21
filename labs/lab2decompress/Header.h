9580cac483f5d899eb41398001ef1a6eda3ac664
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-21 23:40:48 +0300

    Update Header.h

--- labs/lab2decompress/Header.h
+++ labs/lab2decompress/Header.h
@@ -8,5 +8,5 @@ struct imp {
     char* val2;
 };
 
-void print_in_file(FILE* fp, char* val1, const char* val2, const char* add, char c);
+void print_in_file(FILE* fp, char* val1, const char* val2, const char* add);
 void final_step(FILE* f, FILE* fp, struct imp* temp, int ind);
