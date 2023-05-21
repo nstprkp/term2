3bd7fc1f3e549cd11bdb46da1e948dd769790aee
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-21 23:52:34 +0300

    Update Source.c

--- labs/lab2decompress/Source.c
+++ labs/lab2decompress/Source.c
@@ -56,7 +56,9 @@ void final_step(FILE* f, FILE* fp, struct imp* temp, int ind)
             char* istr;
             istr = (char*)calloc(256, sizeof(char));
             if (buff != NULL) {
+                istr = (char*)realloc(istr, (strlen(buff)+1)*sizeof(char));
                 istr = strtok(buff, sep);
+                free(istr);
             }
             int i = 0;
             int z = 0;
