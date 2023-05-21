2f1c70a872e9536827af348775d3978f3a02af42
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-21 21:22:24 +0300

    Update Source.c

--- labs/laba4akinator/Source.c
+++ labs/laba4akinator/Source.c
@@ -5,9 +5,10 @@ char* getStr(char *s, int k)
     char c;
     int chr;
     int i=0;
-    while(--k > 0 && (chr = getchar())!=EOF && c != '\n')
+    while(--k > 0 && (chr = getchar())!=EOF && c != '\n') {
         c = (char)chr;
         s[i++] = c;
+    }
     s[i] = '\0';
     return s;
 }
