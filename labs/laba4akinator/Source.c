a5111d90413c641bfa6689d383c06a655334ef99
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-22 01:17:03 +0300

    Update Source.c

--- labs/laba4akinator/Source.c
+++ labs/laba4akinator/Source.c
@@ -1,12 +1,16 @@
 #include "Header.h"
 
-char* getStr(char *s, int k)
+char* get_str(char* s, int k)
 {
     char c;
-    int chr;
-    int i=0;
-    while(--k > 0 && (chr = getchar())!=EOF && chr != 13) {
-        c = (char)chr;
+    int ch;
+    int i = 0;
+    while (--k > 0) {
+        ch = getchar();
+        if (ch == EOF || ch == 13) {
+            break;
+        }
+        c = (char)ch;
         s[i++] = c;
     }
     s[i] = '\0';
