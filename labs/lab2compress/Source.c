25dea468bf3e04ce610e617b20b6d14559a197e2
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-21 20:33:04 +0300

    Update Source.c

--- labs/lab2compress/Source.c
+++ labs/lab2compress/Source.c
@@ -36,7 +36,7 @@ char* top(struct stack* st) {
     return st[0].value[st[0].pointer];
 }
 
-int counting(struct stack* st1, struct stack* st2, char* onTop, int n, int t)
+int counting(struct stack* st1, struct stack* st2, const char* onTop, int n, int t)
 {
     int cnt = 1;
     pop(st1);
@@ -50,7 +50,7 @@ int counting(struct stack* st1, struct stack* st2, char* onTop, int n, int t)
     return cnt;
 }
 
-void make_struct(struct words* word, char* onTop, int cnt, int kol)
+void make_struct(struct words* word, const char* onTop, int cnt, int kol)
 {
     word[kol].val = (char*)calloc(strlen(onTop)+1, sizeof(char));
     if (word[kol].val != 0) {
@@ -92,7 +92,7 @@ void free_memory(struct stack* st1, struct stack* st2, int n)
     st2 = NULL;
 }
 
-void print_in_file(FILE* fp, char* val1, char* val2, char* add, char c)
+void print_in_file(FILE* fp, char* val1, const char* val2, const char* add, char c)
 {
     if (add != NULL) {
         if (strcmp(val2, add) != 0) {
@@ -137,10 +137,10 @@ void final_step(FILE* f, FILE* fp, struct imp* temp, int ind)
         char sep[16] = "[](),*.;:!?\" ";
         char* istr;
         istr = calloc(256, sizeof(char));
-        char* saveptr = NULL;
         istr = strtok(buff, sep);
 
-        int i = 0, t = 0;
+        int i = 0;
+        int t = 0;
         if (temp != NULL && istr != NULL) {
             while (i < ind && t == 0) {
                 if (strcmp(temp[i].val1, istr) == 0) {
