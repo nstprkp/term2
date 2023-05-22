2739482f8f80a4fd65434f01c2312946e1cb6a63
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-22 16:22:24 +0300

    Update Source.c

--- labs/lab2decompress/Source.c
+++ labs/lab2decompress/Source.c
@@ -23,66 +23,76 @@ void print_in_file(FILE* fp, char* val1, const char* val2, const char* add)
     }
 }
 
-void final_step(FILE* f, FILE* fp, struct imp* temp, int ind)
-{
+void final_step(FILE* f, FILE* fp, struct imp* temp, int ind) {
     int t = 0;
     char buff[256];
     char c = ' ';
-    
+
     while (!feof(f) && t == 0) {
-        char st[2] = "/";
         fscanf(f, "%255s", buff);
-        
-        if (strcmp(buff, st) == 0) {
-            t = 1;
-        }
-        
+        t = check_termination(buff);
         if (t == 0) {
-            if (c != '\0') {
-                if (c == '\n') {
-                    fprintf(fp, "%c%c", c, c);
-                } else if (c == ' ') {
-                    fprintf(fp, "%c", c);
-                }
-            }
-            
-            char* add = calloc(256, sizeof(char));
-            if (add != NULL) {
-                strcpy(add, buff);
-            }
-            
-            c = ' ';
-            int ch;
-            ch = fgetc(f);
-            c = (char)ch;
-            
-            char sep[16] = "[](),*.;:!?\"";
-            const char* istr;
-            if (buff != NULL) {
-                istr = strtok(buff, sep);
-            }
-            
-            int i = 0;
-            int z = 0;
-            
-            if (temp != NULL && istr != NULL) {
-                while (i < ind && z == 0) {
-                    if (strcmp(temp[i].val1, istr) == 0) {
-                        print_in_file(fp, temp[i].val2, temp[i].val1, add);
-                        z++;
-                    } else if (strcmp(istr, temp[i].val2) == 0) {
-                        print_in_file(fp, temp[i].val1, temp[i].val2, add);
-                        z++;
-                    }
-                    i++;
-                }
-                
-                if (z == 0) {
-                    fprintf(fp, "%s", add);
-                }
-            }
-            
-            free(add);
+            process_buffer(f, fp, buff, &c, temp, ind);
         }
     }
+}
+
+int check_termination(char* buff) {
+    char st[2] = "/";
+    if (strcmp(buff, st) == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+void process_buffer(FILE* f, FILE* fp, char* buff, char* c, struct imp* temp, int ind) {
+    if (*c != '\0') {
+        if (*c == '\n') {
+            fprintf(fp, "%c%c", *c, *c);
+        } else if (*c == ' ') {
+            fprintf(fp, "%c", *c);
+        }
+    }
+
+    char* add = calloc(256, sizeof(char));
+    if (add != NULL) {
+        strcpy(add, buff);
+    }
+
+    *c = ' ';
+    int ch;
+    ch = fgetc(f);
+    *c = (char)ch;
+
+    char sep[16] = "[](),*.;:!?\"";
+    const char* istr;
+    if (buff != NULL) {
+        istr = strtok(buff, sep);
+    }
+
+    int i = 0;
+    int z = 0;
+
+    if (temp != NULL && istr != NULL) {
+        while (i < ind && z == 0) {
+            if (strcmp(temp[i].val1, istr) == 0) {
+                print_in_file(fp, temp[i].val2, temp[i].val1, add);
+                z++;
+            } else if (strcmp(istr, temp[i].val2) == 0) {
+                print_in_file(fp, temp[i].val1, temp[i].val2, add);
+                z++;
+            }
+            i++;
+        }
+
+        if (z == 0) {
+            fprintf(fp, "%s", add);
+        }
+    }
+
+    free(add);
+}
+
+void print_in_file(FILE* fp, char* val1, char* val2, char* add) {
+    fprintf(fp, "%s[%s]%s", val1, val2, add);
 }
