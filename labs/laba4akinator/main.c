df24269b01ab90fc5917f92e969c47be6344a6e3
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-24 21:41:34 +0300

    Update main.c

--- labs/laba4akinator/main.c
+++ labs/laba4akinator/main.c
@@ -63,9 +63,9 @@ void play_game(tree_node** tree, int* check) {
         printf("%s!\n\n", str_new);
         free(str_new);
         time_t time_now;
-        char time_string[26];
+        char time_str[26];
         time_now = time(NULL);
-        ctime_r(&time_now, time_string);
+        char* time_string = ctime_r(&time_now, time_str);
         time_string[strlen(time_string) - 1] = '!';
         
         str_new_type = (char*)realloc(str_new_type, (strlen(str_new_type) + strlen(time_string) + 4) * sizeof(char));
