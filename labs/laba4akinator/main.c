4a886f5f1a77ffb2f5d837e606b50a78320c1013
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-27 13:38:07 +0300

    Update main.c

--- labs/laba4akinator/main.c
+++ labs/laba4akinator/main.c
@@ -63,12 +63,10 @@ void play_game(tree_node** tree, int* check) {
         printf("%s!\n\n", str_new);
         free(str_new);
         
-        struct tm result;
-        
         time_t time_now;
         char time_string[80];
         time(&time_now);
-        strftime(time_string, sizeof(time_string), "%c", localtime_r(&time_now, &result));
+        strftime(time_string, sizeof(time_string), "%c", localtime(&time_now));
         
         
      /*   time_t time_now;
