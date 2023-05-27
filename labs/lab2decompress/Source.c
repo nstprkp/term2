46e26e28b974b671650c8c1065eab6679f859a34
Anastasiya Prakapenka <113131930+nstprkp@users.noreply.github.com>
2023-05-27 13:56:20 +0300

    Update Source.c

--- labs/lab2decompress/Source.c
+++ labs/lab2decompress/Source.c
@@ -52,12 +52,9 @@ void process_buffer(FILE* f, FILE* fp, char* buff, char* c, struct imp* temp, in
 
     char sep[16] = "[](),*.;:!?\"";
     char* istr; 
-    // = (char*)calloc(256, sizeof(char));
-  //  if (buff != NULL) {
-        char* saveptr;
-        istr = strtok_r(buff, sep, &saveptr);
- //   }
-//    istr = (char*)realloc(istr, strlen(istr)*sizeof(char));
+    char* saveptr;
+    istr = strtok_r(buff, sep, &saveptr);
+    
     int i = 0;
     int z = 0;
 
